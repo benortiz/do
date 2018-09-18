ce04b3161c8a29eb2d375181110583057a4e1c58
Benjamin Ortiz <hello.ben.ortiz@gmail.com>
2018-09-17 20:28:30 -0700

    Add tests for Taskpaper::Note
    Tests for the basic expected usage of #to_s and #add_line

--- spec/taskpaper/note_spec.rb
+++ spec/taskpaper/note_spec.rb
@@ -1,7 +1,19 @@
 require 'do'
 
 describe 'Taskpaper::Note' do
-  it 'initializes' do
-    expect{Taskpaper::Note.new(task: nil)}.not_to raise_error
+  describe '#to_s' do
+    it 'joins lines in a note with a new line character' do
+      expect(
+        Taskpaper::Note.new(task: nil, lines: ['note', 'note1']).to_s
+      ).to eq("\t\tnote\n\t\tnote1")
+    end
+  end
+
+  describe '#add_line' do
+    it 'appends the line to the lines array' do
+      note = Taskpaper::Note.new(task: nil, lines: ['note'])
+      note.add_line('note1')
+      expect(note.lines).to eq(['note', 'note1'])
+    end
   end
 end
